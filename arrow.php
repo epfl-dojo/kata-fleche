fdd9e97e7ce8b714ffc707c5120e98f99b7c71fa
Nicolas Borboën <ponsfrilus@gmail.com>
2018-10-11 15:26:25 +0200

    Fixes for CLI version
    * `&nbsp;` spaces to ` ` spaces
    * no more need for ? nl2br()` functions
    
    → This allow to run the script in a Command Line Interface as all the
    other proposal, e.g.
    `php arrow.php`.
    
    On a windows operating system, check this out:
    https://stackoverflow.com/questions/15597067/how-to-run-php-from-windows-command-line-in-wampserver

--- arrow.php
+++ arrow.php
@@ -4,15 +4,15 @@ $height = 10;
 
 function line($height){
     for ($i = 1; $i < ($height*2); $i++) {
-        echo "*";
+        echo "* ";
     }
 }
 
 function tige($height) {
-    echo nl2br("\n");
+    echo "\n";
     for($i = 0; $i < $height; $i++) {
-        echo str_repeat('&nbsp', ($height*2)-2);
-        echo nl2br("*\n");
+        echo str_repeat(' ', ($height*2)-2);
+        echo "*\n";
     }
 }
 
@@ -22,13 +22,13 @@ function arrow($height) {
         $nb += 2;
         $space = ($height*2)-$nb;
         if($nb == 2){
-            echo str_repeat('&nbsp', $space);
-            echo nl2br("*\n");
+            echo str_repeat(' ', $space);
+            echo "*\n";
         } else {
-            echo str_repeat('&nbsp', $space);
-            echo nl2br("*");
-            echo str_repeat('&nbsp', $nb*2-6);
-            echo nl2br("*\n");
+            echo str_repeat(' ', $space);
+            echo "*";
+            echo str_repeat(' ', $nb*2-6);
+            echo " *\n";
         }
     }
 }
