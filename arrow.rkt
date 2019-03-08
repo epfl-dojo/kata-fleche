4e2cb9e0476a6edcddc0d2c15a194e55251c4c3d
Loïc Humbert <loichumbert5450@gmail.com>
2019-03-08 15:38:24 +0100

    Voilà qui semble plus propre

--- arrow.rkt
+++ arrow.rkt
@@ -1,42 +1,28 @@
 #lang racket
-;(define a 1)
-;(define b 0)
-;(+ a b)
-;(let ((a 10) (b 2)) (* a b))
-;((λ(a) (+ 1 a)) 2)
 
-;(if (> a b) 'oui 'no)
-
-;(define foo (λ(a (b 0)) (+ 1 a b)))
-;(foo 4)
-
-
-(display "Entrer la taille de la flèche:\n")
-
-(define-values (in out) (make-pipe))
-
+;; Déclarer les fonctions de construction de la flèche
 (define line (λ (size) (make-string size #\*)))
-
 (define tranche-de-flèche (λ (taille-base numéro)
                             (let ((space (- (- taille-base numéro) 1)))
                               (string-append (make-string space #\ )
                                            "*"
                                            (make-string (- (* (- taille-base space) 2) 1) #\ )
                                            "*"))))
-
-
-
-;(write "Size:" out)
-(define size (string->number (read-line (current-input-port))))
 (define display-line (λ(str) (display (string-append str "\n" ))))
 
-(if (exact-integer? size) 'OK 'Interge )
-;(display (string-append  "Dessine une flèche de taille : \n" result))
+;; Récupérer la hauteur de la flèche
+(display-line "Entrer la taille de la flèche:")
+(define-values (in out) (make-pipe))
+(define size (string->number (read-line (current-input-port))))
 
+;; Validation de la hauteur entrée
+(if (exact-integer? size) 'OK ((display-line "Ceci n'est pas un nombre")(exit)))
+(if (or (< size 2) (> size 20))
+    ((display-line "La hauteur de la flèche doit être comprise entre 2 et 20") (exit))
+    (display-line "OK"))
+
+;; Dessiner la flèche
 (display-line (string-append (make-string size #\ ) "*"))
 (for ([i (in-range (- size 1))]) (display-line (tranche-de-flèche size i)))
-
 (display-line (line (+ (* size 2) 1)))
- 
 (for ([i (in-range size)]) (display-line (string-append (make-string size #\ ) "*")))
-
