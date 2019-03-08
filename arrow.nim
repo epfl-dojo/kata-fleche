5021fc5f79e39627f3600600c330a29685f75cbb
Nicolas Borboën <ponsfrilus@gmail.com>
2019-03-08 17:26:26 +0100

    Attempt to fix mess between #20 and #21

--- arrow.nim
+++ arrow.nim
@@ -1,21 +1,43 @@
-import strutils, parseutils
+# HowTo
+#  1. install https://nim-lang.org
+#  2. compile with `nim c arrow.nim`
+#  3. launch with `./arrow`
 
-echo "What size should the arrow be?"
-var size: string = readLine(stdin)
-var sizeInt: int = parseInt(size)
+import os, strutils, parseutils
 
-# spike
+# Get CLI args
+let arguments = commandLineParams()
+var sizeInt: int = 0
+
+proc pUsage(): string =
+  result = "An integer between 2 and 20 is needed\nUsage: ./arrow 4"
+
+try:
+  sizeInt = parseInt(arguments[0])
+except:
+  let
+    e = getCurrentException()
+    msg = getCurrentExceptionMsg()
+  # echo "Got exception ", repr(e), " with message ", msg
+  echo pUsage()
+  quit()
+
+if sizeInt <= 2 or sizeInt >= 20:
+  echo pUsage()
+  quit()
+
+# Draw spike
 echo repeat(" ", sizeInt), "*"
 
-# head
+# Draw head
 var rowSize: int = sizeInt
 for i in 2..sizeInt:
-  echo repeat(" ", rowSize - 1), "*", repeat(" ", (sizeInt - rowSize )* 2 + 1), "*"
+  echo repeat(" ", rowSize - 1), "*", repeat(" ", (sizeInt - rowSize ) * 2 + 1), "*"
   rowSize -= 1
 
-# base
+# Draw base
 echo repeat("*", sizeInt * 2 + 1)
 
-# tail
+# Draw tail
 for i in 1..sizeInt:
   echo repeat(" ", sizeInt), "*"
